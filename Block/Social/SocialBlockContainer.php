32109ac62df35da0cf624934788d32e7e58df145
Hugo Briand <briand@ekino.com>
2014-03-05 17:52:33 +0100

    MopaBootstrapBundle now optional; added SDKs option to social container; fixed notice

--- Block/Social/SocialBlockContainer.php
+++ Block/Social/SocialBlockContainer.php
@@ -11,7 +11,11 @@
 
 namespace Sonata\SeoBundle\Block\Social;
 
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\ContainerBlockService;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 
@@ -20,21 +24,106 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  *
  * @package Sonata\SeoBundle\Block\Social
  *
- * @author Hugo Briand <briand@ekino.com>
+ * @author  Hugo Briand <briand@ekino.com>
  */
 class SocialBlockContainer extends ContainerBlockService
 {
+    /**
+     * Returns the mapping between service and associated sdk template
+     *
+     * @return array
+     */
+    public static function getSdkTemplateMapping()
+    {
+        return array(
+            'facebook'  => "SonataSeoBundle:Block:_facebook_sdk.html.twig",
+            'twitter'   => "SonataSeoBundle:Block:_twitter_sdk.html.twig",
+            'pinterest' => "SonataSeoBundle:Block:_pinterest_sdk.html.twig"
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        $includeTemplates = array();
+
+        if (is_array($sdks = $blockContext->getSetting('load_sdks'))) {
+            $mapping = SocialBlockContainer::getSdkTemplateMapping();
+            $mappingKeys = array_keys($mapping);
+
+            foreach ($sdks as $sdk) {
+                $includeTemplates[] = $mapping[$mappingKeys[$sdk]];
+            }
+        }
+
+        return $this->renderResponse(
+            $blockContext->getTemplate(),
+            array(
+                'block'             => $blockContext->getBlock(),
+                'decorator'         => $this->getDecorator($blockContext->getSetting('layout')),
+                'settings'          => $blockContext->getSettings(),
+                'include_templates' => $includeTemplates
+            ),
+            $response
+        );
+    }
+
     /**
      * {@inheritdoc}
      */
     public function setDefaultSettings(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'code'        => '',
-            'layout'      => '{{ CONTENT }}',
-            'class'       => '',
-            'template'    => 'SonataSeoBundle:Block:block_social_container.html.twig',
+        $resolver->setDefaults(
+            array(
+                'code'      => '',
+                'layout'    => '{{ CONTENT }}',
+                'class'     => '',
+                'template'  => 'SonataSeoBundle:Block:block_social_container.html.twig',
+                'load_sdks' => array(),
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formMapper->add('enabled');
+
+        $formMapper->add(
+            'settings',
+            'sonata_type_immutable_array',
+            array(
+                'keys' => array(
+                    array('layout', 'textarea', array()),
+                    array('class', 'text', array('required' => false)),
+                    array(
+                        'load_sdks', 'choice', array(
+                            'required'   => false,
+                            'multiple'   => true,
+                            'choices'    => array_keys(SocialBlockContainer::getSdkTemplateMapping()),
+                        )
+                    ),
+                )
+            ),
+            array('help' => "The SDK must be added once and only once on each page for the services you wish to use")
+        );
+
+        $formMapper->add('children', 'sonata_type_collection', array(), array(
+            'edit'     => 'inline',
+            'inline'   => 'table',
+            'sortable' => 'position'
         ));
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return parent::getName() ? : "Social Blocks Container";
+    }
 }
