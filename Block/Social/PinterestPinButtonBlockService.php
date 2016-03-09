1e68517f2951fa3bf5d7ec532df4d244c01a7a78
Cyril Mizzi <cyril@lesiteimmo.com>
2016-03-09 13:05:10 +0100

    fixes PHP coding standards fixer

--- Block/Social/PinterestPinButtonBlockService.php
+++ Block/Social/PinterestPinButtonBlockService.php
@@ -49,32 +49,18 @@ class PinterestPinButtonBlockService extends BaseBlockService
     {
         $formMapper->add('settings', 'sonata_type_immutable_array', array(
             'keys' => array(
-                array('url', 'url', array(
-                    'required' => false,
-                    'label'    => 'form.label_url',
-                )),
-                array('image', 'text', array(
-                    'required' => false,
-                    'label'    => 'form.label_image',
-                )),
-                array('description', 'text', array(
-                    'required' => false,
-                    'label'    => 'form.label_description',
-                )),
-                array('size', 'integer', array(
-                    'required' => false,
-                    'label'    => 'form.label_size',
-                )),
+                array('url', 'url', array('required' => false)),
+                array('image', 'text', array('required' => false)),
+                array('description', 'text', array('required' => false)),
+                array('size', 'integer', array('required' => false)),
                 array('shape', 'choice', array(
                     'required' => false,
                     'choices'  => array(
-                        'rectangular' => 'form.label_shape_rectangular',
-                        'round'       => 'form.label_shape_round',
+                        'rectangular' => 'rectangular',
+                        'round'       => 'round',
                     ),
-                    'label' => 'form.label_shape',
                 )),
             ),
-            'translation_domain' => 'SonataSeoBundle',
         ));
     }
 
