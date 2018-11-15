73d62ab88c86aecadfe82a60cbf9809a0d7c7b30
Sullivan SENECHAL <soullivaneuh@gmail.com>
2018-11-15 02:07:04 +0100

    Applied fixes from FlintCI

--- tests/Block/Social/TwitterEmbedTweetBlockServiceTest.php
+++ tests/Block/Social/TwitterEmbedTweetBlockServiceTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -20,7 +22,7 @@ use Sonata\SeoBundle\Tests\Fixtures\Block\TwitterEmbedTweetBSTest;
  */
 class TwitterEmbedTweetBlockServiceTest extends AbstractBlockServiceTestCase
 {
-    public function testBuildUri()
+    public function testBuildUri(): void
     {
         $settings = [
             'tweet' => 'tweeeeeeeet',
--- tests/DependencyInjection/SonataSeoExtensionTest.php
+++ tests/DependencyInjection/SonataSeoExtensionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -25,7 +27,7 @@ class SonataSeoExtensionTest extends TestCase
     /**
      * Tests the loading of blocks.xml file.
      */
-    public function testBlocksLoading()
+    public function testBlocksLoading(): void
     {
         $container = new ContainerBuilder();
         $container->setParameter('kernel.bundles', [
