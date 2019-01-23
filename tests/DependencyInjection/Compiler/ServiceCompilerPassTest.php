7c633a778df3dba5eb4e319eceb488dcfa3c9989
Leroy Baeyens <leroy.baeyens@bax-shop.nl>
2019-01-23 13:56:15 +0100

    Added the SeoPageInterface to the container as an alias. This should fix #320.

---
+++ tests/DependencyInjection/Compiler/ServiceCompilerPassTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\SeoBundle\Tests\DependencyInjection\Compiler;
+
+use PHPUnit\Framework\TestCase;
+use Sonata\SeoBundle\DependencyInjection\Compiler\ServiceCompilerPass;
+use Sonata\SeoBundle\DependencyInjection\SonataSeoExtension;
+use Sonata\SeoBundle\Seo\SeoPageInterface;
+use Sonata\SeoBundle\Tests\Stubs\SeoPageStub;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ServiceCompilerPassTest extends TestCase
+{
+    public function testServicesExistsAndCanBeOverridden()
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.bundles', []);
+
+        $container->register('sonata.seo.custom.page', SeoPageStub::class);
+
+        $config = [
+            'page' => [
+                'default' => 'sonata.seo.custom.page'
+            ]
+        ];
+
+        $extension = new SonataSeoExtension();
+        $extension->load([$config], $container);
+
+        (new ServiceCompilerPass())->process($container);
+
+        $this->assertTrue($service = $container->has('sonata.seo.page'));
+        $this->assertTrue($alias = $container->has(SeoPageInterface::class));
+        $this->assertSame($service, $alias);
+
+        $this->assertInstanceOf(SeoPageStub::class, $container->get(SeoPageInterface::class));
+    }
+}
