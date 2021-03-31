b735124311c0530d06a46c3e8e92ba4bbe657a66
Sullivan SENECHAL <soullivaneuh@gmail.com>
2021-03-31 09:48:33 +0200

    Applied fixes from FlintCI

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -64,8 +64,7 @@ final class Configuration implements ConfigurationInterface
                         ->end()
                     ->end()
                 ->end()
-            ->end()
-        ;
+            ->end();
 
         $this->addHttpClientSection($rootNode);
 
