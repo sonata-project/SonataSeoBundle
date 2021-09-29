0f0679022caf0eecf664931a793babb596b212a0
core23 <mail@core23.de>
2021-09-29 20:03:05 +0200

    Extract Source value object

--- src/Sitemap/SourceManager.php
+++ src/Sitemap/SourceManager.php
@@ -13,18 +13,17 @@ declare(strict_types=1);
 
 namespace Sonata\SeoBundle\Sitemap;
 
-use Sonata\Exporter\Source\ChainSourceIterator;
 use Sonata\Exporter\Source\SourceIteratorInterface;
 
 /**
  * Manager several chain source iterator grouped.
  *
- * @phpstan-implements \Iterator<\stdClass>
+ * @phpstan-implements \Iterator<string, Source>
  */
 final class SourceManager implements \Iterator
 {
     /**
-     * @var \ArrayIterator<string, \stdClass>
+     * @var \ArrayIterator<string, Source>
      */
     private \ArrayIterator $sources;
 
@@ -41,16 +40,13 @@ final class SourceManager implements \Iterator
     public function addSource(string $group, SourceIteratorInterface $source, array $types = []): void
     {
         if (!isset($this->sources[$group])) {
-            $this->sources[$group] = new \stdClass();
-
-            $this->sources[$group]->sources = new ChainSourceIterator();
-            $this->sources[$group]->types = [];
+            $this->sources[$group] = new Source();
         }
 
-        $this->sources[$group]->sources->addSource($source);
+        $this->sources[$group]->addSource($source);
 
         if ([] !== $types) {
-            $this->sources[$group]->types += array_diff($types, $this->sources[$group]->types);
+            $this->sources[$group]->addTypes($types);
         }
     }
 
