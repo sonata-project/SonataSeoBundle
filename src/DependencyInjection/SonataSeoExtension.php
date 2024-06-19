4a3bf005a06cefa6de950ddcf6c8e23b5eac7b48
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-06-19 12:38:34 +0200

    Solve usage of internal class

--- src/DependencyInjection/SonataSeoExtension.php
+++ src/DependencyInjection/SonataSeoExtension.php
@@ -18,9 +18,9 @@ use Sonata\Exporter\Source\SymfonySitemapSourceIterator;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
